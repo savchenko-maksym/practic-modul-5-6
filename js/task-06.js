b68c0cde79eba906b8de12833a480e4462437b3d
Maryna <tromsa3122@mail.com>
2025-01-11 15:23:45 +0200

    add

--- js/task-06.js
+++ js/task-06.js
@@ -1,2 +1,13 @@
 // Напишіть функцію makeCounter, яка повертає іншу функцію,
 //     яка рахує та виводить у консоль кількість своїх викликів
+
+
+function makeCounter (count) {
+  return () => console.log(count += 5 );
+}
+const item = makeCounter(10);
+
+item();
+item();
+item();
+item();
--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,11 @@
 // Напишіть функцію savePassword(password), яка приймає пароль та повертає внутрішню функцію,
 //     яка приймає рядок та повертає true, якщо рядок співпадає зі збереженим паролем,
 //     та false - якщо не співпадає
+
+function savePassword(password) {
+  return (userPassword) => password === userPassword;
+    
+}
+const checkPassword = savePassword(1234);
+console.log(checkPassword(4567));
+console.log(checkPassword(1234));
